f8bbbaa60a6da590b22ab10b26f03bd1c6276159
Fabricio Toresan <fabricio.toresan@gmail.com>
2017-07-25 20:43:27 -0300

    Files for the blog app

---
+++ src/components/posts_new.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input 
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+  
+  render() {
+    return (
+      <form>
+        <Field
+          name="title"
+          label="Title"
+          component={this.renderField}
+        />
+        <Field 
+          name="tags"
+          label="Tags"
+          component={this.renderField}
+        />
+        <Field 
+          name="content"
+          label="Post Content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
