e7adaad4c9c1763c535e6dd2fb3c248405a4e67d
Fabricio Toresan <fabricio.toresan@gmail.com>
2017-08-01 20:26:58 -0300

    Component to show a single post, and actionCreator to fetch a post from the backend

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,7 +18,8 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
-          <Route path="/" component={PostsIndex} />          
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
     </BrowserRouter>
